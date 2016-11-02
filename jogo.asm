6318158d4401b07b9360d47dd7de223661d4a357
Paulo Figueiredo <paulo_jof@live.com>
2016-11-02 10:33:01 -0200

    Iniciada a configuração do programa
    + Inicialização de video (320x200)
    + Finalização do programa

--- jogo.asm
+++ jogo.asm
@@ -2,12 +2,23 @@ TITLE SpaceInvaders
 .MODEL SMALL
 .STACK 100H
 .DATA
-	
+	ALIEN1	DW	0
 .CODE            
 
-
+;===================================================================================================================================
+;			PROCESSO PRINCIPAL - SETAR VIDEO, FECHAR PROGRAMA E AFINS
+;===================================================================================================================================
 MAIN PROC
-		
+
+	MOV	AL,	13h		;Inicialização de video
+	MOV	AH,	0
+	INT	10h
+	
+	
+	
+	MOV	AH,	4CH		;Finalização do programa
+	INT	21H
+	
 
 
 MAIN ENDP
